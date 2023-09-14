27721366956e2dec74d84207726302ba6a240c37
Robotechnic <robotechnic.developeur@gmail.com>
2023-09-14 14:58:47 +0200

    base project

---
+++ M99.py
@@ -0,0 +1,472 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+M99 Machine
+"""
+import re
+
+# Token format: (regex, (opcode, arg1, arg2, ...))
+# if argument is negative, it refer to a regex group
+# else it is a constant
+REGISTER_TOKENS = "(A|B|R|PC|SB|RA)"
+TOKENS = [
+    (r"^\s*STR ([0-9]{1,2})$", (0, 0, -1)),
+    (r"^\s*LDA ([0-9]{1,2})$", (1, 0, -1)),
+    (r"^\s*LDB ([0-9]{1,2})$", (2, 0, -1)),
+    (r"^\s*MOV " + REGISTER_TOKENS + " " + REGISTER_TOKENS + "$", (3, -1, -2)),
+    (r"^\s*ADD$", (4, 0, 0)),
+    (r"^\s*SUB$", (4, 0, 1)),
+    (r"^\s*MUL$", (4, 0, 2)),
+    (r"^\s*JMP ([0-9]{1,2})$", (5, 0, -1)),
+    (r"^\s*JPP ([0-9]{1,2})$", (6, 0, -1)),
+    (r"^\s*JEQ ([0-9]{1,2})$", (7, 0, -1)),
+    (r"^\s*JNE ([0-9]{1,2})$", (8, 0, -1)),
+    (r"^\s*CAL ([0-9]{1,2})$", (9, 0, -1)),
+    (r"^\s*RET$", (4, 0, 9)),
+    (r"^\s*PSH " + REGISTER_TOKENS + "$", (4, 8, -1)),
+    (r"^\s*POP " + REGISTER_TOKENS + "$", (4, 9, -1)),
+    (r"^\s*DAT ([0-9]{1,3})$", (0, 0, -1)),
+]
+
+
+class M99:
+    def __init__(self) -> None:
+        self.update_event = None
+        self.mem = [0] * 99
+        self.read_value = M99.read_value
+        self.write_value = M99.write_value
+        self.restart()
+
+    def restart(self) -> None:
+        self.reg = [
+            0,  # R
+            0,  # A
+            0,  # B
+            0,  # PC
+            98,  # SB
+            0,  # RA
+        ]
+        self._shutdown = False
+        self.emit_update_event()
+    
+    def shutdown(self) -> None:
+        self._shutdown = True
+        self.emit_update_event()
+
+    def after_exec(self, callback: callable) -> None:
+        """
+        Set the callback to be executed after each instruction.
+
+        Args:
+            callback (callable): callback to be executed after each instruction.
+        """
+        self.update_event = callback
+
+    @staticmethod
+    def read_value() -> int:
+        """
+        Read a value from the standard input.
+
+        Returns:
+            int: value read from the standard input.
+        """
+        print("Enter a value: ", end="")
+        valid = False
+        while not valid:
+            value = input()
+            if value.isnumeric():
+                valid = True
+            else:
+                print("Invalid input. Enter a value: ", end="")
+        return int(value)
+
+    @staticmethod
+    def write_value(value: int) -> None:
+        """
+        Write a value to the standard output.
+
+        Args:
+            value (int): value to be written to the standard output.
+        """
+        print(value)
+
+    @staticmethod
+    def reg_to_id(reg: str) -> int:
+        """
+        Convert the given register to its corresponding id.
+
+        Args:
+            reg (str): register to be converted.
+
+        Returns:
+            int: id of the given register.
+        """
+        match reg:
+            case "R":
+                return 0
+            case "A":
+                return 1
+            case "B":
+                return 2
+            case "PC":
+                return 3
+            case "SB":
+                return 4
+            case "RA":
+                return 5
+            case _:
+                raise ValueError("Invalid register.")
+
+    @staticmethod
+    def id_to_reg(id: int) -> str:
+        """
+        Convert the given register id to its corresponding register name.
+
+        Args:
+            id (int): register id to be converted.
+
+        Returns:
+            str: the corresponding register name.
+        """
+        match id:
+            case 0:
+                return "R"
+            case 1:
+                return "A"
+            case 2:
+                return "B"
+            case 3:
+                return "PC"
+            case 4:
+                return "SB"
+            case 5:
+                return "RA"
+            case _:
+                raise ValueError("Invalid register id.")
+
+    @staticmethod
+    def manage_overflow(value: int) -> int:
+        """
+        Make sure the given value is not greater than 999 and not less than -999
+        by cycling it.
+
+        Args:
+            value (int): value to be managed.
+
+        Returns:
+            int: managed value.
+        """
+        while value > 999:
+            value -= 1999
+
+        while value < -999:
+            value += 1999
+
+        return value
+
+    def exec_reg_op(self, opcode: int) -> None:
+        match opcode:
+            case 0:
+                self.reg[0] = self.reg[1] + self.reg[2]
+            case 1:
+                self.reg[0] = self.reg[1] - self.reg[2]
+            case 2:
+                self.reg[0] = self.reg[1] * self.reg[2]
+            case 9:
+                self.reg[3] = self.reg[5] - 1
+            case _:
+                if opcode < 80:
+                    raise ValueError("Invalid register operation.")
+
+                self.stack_op(opcode)
+
+        self.reg[0] = M99.manage_overflow(self.reg[0])
+
+    def stack_op(self, opcode: int) -> None:
+        """
+        Execute a stack operation.
+
+        Args:
+            opcode (int): opcode to be executed in the form of a 3-digit integer.
+        """
+        stack_op = opcode // 10
+        reg = opcode % 10
+        if stack_op == 8:
+            if self.reg[4] <= 0:
+                raise ValueError("Stack overflow")
+            self[self.reg[4]] = self.reg[reg]
+            self.reg[4] -= 1
+        elif stack_op == 9:
+            if self.reg[4] >= 98:
+                raise ValueError("Stack is empty.")
+            self.reg[4] += 1
+            self.reg[reg] = self[self.reg[4]]
+
+    def __exec(self, opcode: int) -> None:
+        """
+        Execute the given opcode in the M99 machine.
+
+        Args:
+            opcode (int): opcode to be executed in the form of a 3-digit integer.
+        """
+
+        if opcode > 999 or opcode < 0:
+            raise ValueError("Opcode must be a 3-digit integer.")
+
+        data = opcode % 100
+        identifier = opcode // 100
+
+        match identifier:
+            case 0:
+                self[data] = self.reg[0]
+            case 1:
+                self.reg[1] = self[data]
+            case 2:
+                self.reg[2] = self[data]
+            case 3:
+                reg1 = data // 10
+                reg2 = data % 10
+                self.reg[reg2] = self.reg[reg1]
+            case 4:
+                self.exec_reg_op(data)
+            case 5:
+                self.reg[3] = data
+            case 6:
+                if self.reg[0] > 0:
+                    self.reg[3] = data - 1
+            case 7:
+                if self.reg[0] == 0:
+                    self.reg[3] += 1
+            case 8:
+                if self.reg[0] != 0:
+                    self.reg[3] += 1
+            case 9:
+                self.reg[5] = self.reg[3] + 1
+                self.reg[3] = data
+            case _:
+                raise ValueError("Invalid identifier.")
+
+    def load(self, program: list[int], offset: int = 0) -> None:
+        """
+        Load a program into the M99 machine.
+
+        Args:
+            program (list[int]): program to be loaded into the M99 machine.
+            offset (int): base memory address to load the program into.
+        """
+
+        if len(program) + offset > 98:
+            raise ValueError("Program too long.")
+
+        self.mem[offset : len(program) + offset] = program
+
+    def __getitem__(self, key: int) -> int:
+        """
+        Get the value of the memory cell at the given index.
+
+        Args:
+            key (int): index of the memory cell.
+
+        Returns:
+            int: value of the memory cell at the given index.
+        """
+
+        if key > 99 or key < 0:
+            raise ValueError("Memory cell index out of range.")
+
+        if key == 99:
+            value = self.read_value()
+            if value is None:
+                self.shutdown()
+                raise ValueError("Invalid input.")
+            return M99.manage_overflow(value)
+
+        return self.mem[key]
+
+    def __setitem__(self, key: int, value: int) -> None:
+        """
+        Set the value of the memory cell at the given index.
+
+        Args:
+            key (int): index of the memory cell.
+            value (int): value to be set.
+        """
+
+        if key > 99 or key < 0:
+            raise ValueError("Memory cell index out of range.")
+
+        value = M99.manage_overflow(value)
+
+        if key == 99:
+            self.write_value(value)
+            return
+
+        self.mem[key] = value
+
+    def step(self) -> None:
+        """
+        Execute the instruction located in SP and increment it
+        """
+        if self._shutdown:
+            return
+
+        self.__exec(self.mem[self.reg[3]])
+
+        self.reg[3] += 1
+        if self.reg[3] >= 99:
+            self._shutdown = True
+
+        self.emit_update_event()
+
+    def emit_update_event(self) -> None:
+        if self.update_event:
+            self.update_event()
+
+    def run(self, offset: int = 0) -> None:
+        """
+        Run the program loaded into the M99 machine.
+
+        Args:
+            offset (int): base memory address to start the program from.
+        """
+        if offset > 0:
+            self.reg[3] = offset
+
+        while not self._shutdown:
+            self.step()
+
+def search_match(
+    line: str, line_nb: int
+) -> tuple[re.Match, tuple[int, int, int]]:
+    """
+    Search for a match in the given line.
+
+    Args:
+        line (str): line to be searched.
+        line_nb (int): line number.
+
+    Returns:
+        tuple[re.Pattern, tuple[int, int, int]]: match object and opcode.
+    """
+    for token in TOKENS:
+        match = re.match(token[0], line)
+        if match:
+            return (match, token[1])
+
+    raise ValueError(f"Invalid instruction at line {line_nb} : {line}")
+
+
+def scan_labels(labels: dict[str, int], lines: list[str]) -> str:
+    """
+    Scan the code to find labels and return the code without labels
+    This function also remove empty lines
+
+    Args:
+        labels (dict[str, int]): The labels dict
+        lines (list[str]): list of lines representing the assembly code
+
+    Returns:
+        str: the code without labels
+    """
+    code = []
+    address = 0
+    for l in lines:
+        if re.match(r"^\s*$", l):
+            continue
+
+        if m := re.match(r"^\s*:([A-Za-z][a-zA-Z09_]+)$", l):
+            labels[m.group(1)] = address
+        else:
+            code.append(l)
+            address += 1
+    return code
+
+def assemble(code: str) -> list[int]:
+    """
+    Assemble the given code into a program for the M99 machine.
+
+    Args:
+        code (str): code to be assembled.
+
+    Returns:
+        list[int]: assembled program.
+    """
+
+    program = []
+    labels = {}
+    lines = code.split("\n")
+    lines = scan_labels(labels, lines)
+    print(labels)
+    
+    line_nb = 0
+    
+    for line in lines:
+        #skip empty lines
+        line_nb += 1
+        
+        line = replace_labels(labels, line)
+        
+        (instruction_match, token) = search_match(line, line_nb)
+        if token is None:
+            continue
+        opcode = token[0]
+        for i in range(1, len(token)):
+            opcode *= 10
+            if token[i] < 0:
+                data = instruction_match.group(-token[i])
+                if data.isnumeric():
+                    opcode += int(data)
+                else:
+                    opcode += M99.reg_to_id(data)
+            else:
+                opcode += token[i]
+
+        program.append(opcode)
+
+    return program
+
+def replace_labels(labels: dict[str, int], line: str) -> str:
+    """
+    Replace labels in the given line.
+
+    Args:
+        labels (dict[str, int]): labels dict.
+        line (str): line to be processed.
+    """
+    
+
+    for m in re.finditer(r"@([a-zA-Z][a-zA-Z0-9_]*)", line):
+        print(m.group(1))
+        line = line.replace(m.group(0), str(labels[m.group(1)]))
+
+    return line
+
+if __name__ == "__main__":
+    assembly = assemble(
+        """
+    LDA 99
+    PSH A
+    LDA 99
+    PSH A
+    LDA 99
+    PSH A
+    CAL @comp
+    CAL @comp
+    POP R
+    STR 99
+    JMP 99
+    :comp
+    POP A
+    POP B
+    SUB
+    JPP 81
+    PSH A
+    RET
+    PSH B
+    RET
+    """
+    )
+    pc = M99()
+    pc.load(assembly, 40)
+    pc.run(64)
---
+++ M99Interface.py
@@ -0,0 +1,317 @@
+import M99
+from tkinter import Frame, Label, Tk, Button, Widget, LabelFrame
+from tkinter.messagebox import showinfo, showerror
+from tkinter.simpledialog import askinteger
+from tkinter.filedialog import askopenfilename
+
+# 1 means the instruction takes one argument and it's not a register
+# 2 means the instruction takes two registers as arguments
+# 0 means the instruction takes no argument
+# -1 means the instruction takes one argument but it's a register
+
+INSTRUCTIONS_REPR = {
+    0: ("STR", 1),
+    1: ("LDA", 1),
+    2: ("LDB", 1),
+    3: ("MOV", 2),
+    4: {
+        0: {0: ("ADD", 0), 1: ("SUB", 0), 2: ("MUL", 0), 9: ("RET", 0)},
+        8: ("PSH", -1),
+        9: ("POP", -1),
+    },
+    5: ("JMP", 1),
+    6: ("JPP", 1),
+    7: ("JEQ", 1),
+    8: ("JNE", 1),
+    9: ("CAL", 1),
+}
+
+
+class MemoryCell(Frame):
+    def __init__(self, master: Widget, value: int, **kwargs) -> None:
+        super().__init__(
+            master,
+            borderwidth=1,
+            relief="solid",
+            padx=8,
+            pady=3,
+            **kwargs,
+        )
+        self.grid_propagate(0)
+        self.value = value
+        self.create_widgets()
+        self.set_bg(kwargs.get("bg", "white"))
+
+    @staticmethod
+    def opcode_to_str(opcode: int) -> str:
+        """
+        Convert an opcode to its string representation.
+
+        Args:
+            opcode (int): The opcode to convert.
+
+        Returns:
+            str: The string representation of the opcode.
+        """
+        if opcode == 0:
+            return ""
+
+        opcode_len = 3
+        opcode_repr = INSTRUCTIONS_REPR
+        while opcode_len > 0:
+            opcode_len -= 1
+            digit = opcode // (10 ** (opcode_len))
+            opcode = opcode % (10**opcode_len)
+            if digit not in opcode_repr:
+                return ""
+
+            opcode_repr = opcode_repr[digit]
+            if isinstance(opcode_repr, tuple):
+                match opcode_repr[1]:
+                    case 0:
+                        return opcode_repr[0]
+                    case 1:
+                        return f"{opcode_repr[0]} {opcode}"
+                    case 2:
+                        return f"{opcode_repr[0]} {M99.M99.id_to_reg(opcode // 10)} {M99.M99.id_to_reg(opcode % 10)}"
+                    case -1:
+                        return f"{opcode_repr[0]} {M99.M99.id_to_reg(opcode % 10)}"
+                    case _:
+                        raise ValueError("Invalid representation")
+
+    def create_widgets(self) -> None:
+        """
+        Create the MemoryCell based on the value.
+        It represent the value and if not null, the corresponding instruction
+        """
+        self.value_label = Label(
+            self, text=f"{self.value}", font=("Monospace", 20), width=4
+        )
+        self.instruction_label = Label(
+            self,
+            text=f"{self.opcode_to_str(self.value)}",
+            font=("Monospace", 10),
+            width=6,
+        )
+        self.value_label.pack(side="top")
+        self.instruction_label.pack(side="bottom")
+
+    def set_opcode(self, opcode: int) -> None:
+        """
+        Set the opcode of the cell.
+
+        Args:
+            opcode (int): The opcode to set.
+        """
+        self.value = opcode
+        self.value_label["text"] = f"{self.value}"
+        self.instruction_label["text"] = f"{self.opcode_to_str(self.value)}"
+
+    def set_bg(self, bg: str) -> None:
+        """
+        Set the background color of the cell.
+
+        Args:
+            bg (str): The background color to set.
+        """
+        self.value_label.config(bg=bg)
+        self.instruction_label.config(bg=bg)
+
+    def config(self, **kwargs) -> None:
+        """
+        Configure the cell.
+
+        Args:
+            **kwargs: The configuration to set.
+        """
+        super().config(**kwargs)
+        if "bg" in kwargs:
+            self.set_bg(kwargs["bg"])
+
+
+class M99Interface(Frame):
+    def __init__(self, master: Tk, machine: M99.M99) -> None:
+        super().__init__(master)
+        self.machine = machine
+        self.machine.after_exec(self.update_display)
+        self.machine.read_value = self.input_value
+        self.machine.write_value = self.display_value
+        self.pack()
+        self.assembly = []
+        self.create_widgets()
+
+    def build_register_display(self) -> LabelFrame:
+        """
+        Build the display for the registers.
+        """
+        registers = LabelFrame(self, text="Registers")
+        self.registers_labels = ["R", "A", "B", "PC", "SB", "RA"]
+        self.reg_labels = []
+        for i, reg in enumerate(self.registers_labels):
+            self.reg_labels.append(
+                Label(
+                    registers,
+                    text=f"{self.machine.reg[i]}",
+                    height=2,
+                    font=("Monospace", 20),
+                )
+            )
+            Label(registers, text=f"{reg}:", height=2, font=("Monospace", 20)).grid(
+                row=i, column=0
+            )
+            self.reg_labels[i].grid(row=i, column=1)
+
+        return registers
+
+    def display_value(self, value: int) -> None:
+        """
+        Display the given value.
+        """
+        print(value)
+        showinfo("Value", f"The computer emit the value: {value}", parent=self)
+
+    def input_value(self) -> int:
+        """
+        Input a value.
+        """
+        value = askinteger("Value", "Enter a value:", parent=self)
+        print(f"Input: {value}")
+        return value
+
+    def update_register_display(self) -> None:
+        """
+        Update the register display.
+        """
+        for i, reg in enumerate(self.registers_labels):
+            self.reg_labels[i]["text"] = f"{self.machine.reg[i]}"
+
+    def build_memory_display(self) -> LabelFrame:
+        """
+        In format of 10 rows of 10 columns.
+        """
+        memory = LabelFrame(self, text="Memory")
+        self.mem_labels = []
+        for i in range(10):
+            Label(memory, text=f"{i * 10}", width=4).grid(row=0, column=i + 1)
+            row = []
+            for j in range(10):
+                Label(memory, text=f"{j}", width=4).grid(row=j + 1, column=0)
+                if i == 9 and j == 9:
+                    row.append(Label(memory, text="I/O"))
+                else:
+                    bg = "white"
+                    if self.machine.reg[4] == i * 10 + j:
+                        bg = "lightgreen"
+                    elif self.machine.reg[3] == i * 10 + j:
+                        bg = "lightblue"
+                    row.append(MemoryCell(memory, self.machine.mem[i * 10 + j], bg=bg))
+                row[j].grid(row=j + 1, column=i + 1)
+            self.mem_labels.append(row)
+
+        return memory
+
+    def update_memory_display(self) -> None:
+        """
+        Update the memory display.
+        """
+        for i in range(10):
+            for j in range(10):
+                if i == 9 and j == 9:
+                    continue
+                bg = "white"
+                if self.machine.reg[4] == i * 10 + j:
+                    bg = "lightgreen"
+                elif self.machine.reg[3] == i * 10 + j:
+                    bg = "lightblue"
+                self.mem_labels[i][j].set_opcode(self.machine.mem[i * 10 + j])
+                self.mem_labels[i][j].config(bg=bg)
+
+    def build_buttons(self) -> Frame:
+        """
+        Build the buttons.
+        """
+        buttons = Frame(self)
+        Button(buttons, text="Jump", command=self.jump).grid(row=0, column=0)
+        Button(buttons, text="Execute", command=self.run_machine).grid(row=0, column=1)
+        Button(buttons, text="Step", command=self.next_instruction).grid(
+            row=0, column=2
+        )
+        Button(buttons, text="Reset", command=self.machine.restart).grid(
+            row=1, column=0
+        )
+        Button(buttons, text="Load", command=self.load).grid(row=1, column=1)
+        Button(buttons, text="Quit", command=self.quit).grid(row=1, column=2)
+        return buttons
+
+    def next_instruction(self) -> None:
+        """
+        Execute the next instruction.
+        """
+        try:
+            self.machine.step()
+        except Exception as e:
+            showerror("Error", f"An error occurred: {e}", parent=self)
+
+    def run_machine(self) -> None:
+        """
+        Run the machine.
+        """
+        try:
+            self.machine.run(-1)
+        except Exception as e:
+            showerror("Error", f"An error occurred: {e}", parent=self)
+
+    def jump(self) -> None:
+        """
+        Jump to a specific address.
+        """
+        address = askinteger(
+            "Jump", "Enter an address:", parent=self, minvalue=0, maxvalue=99
+        )
+        self.machine.reg[3] = address
+        self.update_display()
+
+    def load(self) -> None:
+        """
+        Load a program.
+        """
+        program_path = askopenfilename(
+            title="Load a program", filetypes=[("M99 Program", "*.m99")]
+        )
+        if not program_path:
+            return
+
+        with open(program_path, "r") as program_file:
+            program = program_file.read()
+
+        try:
+            self.assembly = M99.M99.assemble(program)
+            self.machine.load(self.assembly, 0)
+            self.update_display()
+        except Exception as e:
+            showerror("Error", f"An error occurred:\n {e}", parent=self)
+
+    def create_widgets(self) -> None:
+        """
+        Build the interface to debug the M99 machine.
+        It displays registers, memory, and the stack.
+        A button is provided to execute the next instruction.
+        """
+        self.build_register_display().grid(row=0, column=0)
+        self.build_memory_display().grid(row=0, column=1, rowspan=2)
+        self.build_buttons().grid(row=1, column=0)
+
+    def update_display(self) -> None:
+        """
+        Update the display.
+        """
+        self.update_register_display()
+        self.update_memory_display()
+        self.update()
+
+
+if __name__ == "__main__":
+    root = Tk()
+    pc = M99.M99()
+    interface = M99Interface(root, pc)
+    interface.mainloop()
